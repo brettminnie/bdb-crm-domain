341d328bbb14db2d92f2473addfa4be28c3b7453
Brett Minnie <github@bdb-studios.co.uk>
2014-03-30 21:05:19 +0100

    Updated unit tests

---
+++ tests/Bootstrap.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace ApplicationTest;
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Zend\Loader\AutoloaderFactory;
+use Zend\Mvc\Service\ServiceManagerConfig;
+use Zend\ServiceManager\ServiceManager;
+use RuntimeException;
+
+error_reporting(E_ALL | E_STRICT);
+chdir(__DIR__);
+
+/**
+ * Test bootstrap, for setting up autoloading
+ */
+class Bootstrap
+{
+    protected static $serviceManager;
+
+    public static function init()
+    {
+        $zf2ModulePaths = array(dirname(dirname(__DIR__)));
+        if (($path = static::findParentPath('vendor'))) {
+            $zf2ModulePaths[] = $path;
+        }
+        if (($path = static::findParentPath('module')) !== $zf2ModulePaths[0]) {
+            $zf2ModulePaths[] = $path;
+        }
+
+        static::initAutoloader();
+
+    }
+
+    public static function chroot()
+    {
+        $rootPath = dirname(static::findParentPath('src'));
+        chdir($rootPath);
+    }
+
+    public static function getServiceManager()
+    {
+        return static::$serviceManager;
+    }
+
+    protected static function initAutoloader()
+    {
+        $vendorPath = static::findParentPath('vendor');
+        if (file_exists($vendorPath . '/autoload.php')) {
+            $loader = include $vendorPath . '/autoload.php';
+            $loader->add('ApplicationTest', 'module/Application/test/');
+        }
+
+        AnnotationRegistry::registerLoader('class_exists');
+    }
+
+    protected static function findParentPath($path)
+    {
+        $dir         = __DIR__;
+        $previousDir = '.';
+        while (!is_dir($dir . '/' . $path)) {
+            $dir = dirname($dir);
+            if ($previousDir === $dir) {
+                return false;
+            }
+            $previousDir = $dir;
+        }
+
+        return $dir . '/' . $path;
+    }
+}
+
+Bootstrap::init();
+Bootstrap::chroot();
--- tests/Core/Exceptions/GUIDExceptionTest.php
+++ tests/Core/Exceptions/GUIDExceptionTest.php
@@ -1,11 +1,13 @@
 <?php
 
-class GUIDExceptionTest extends PHPUnit_Framework_TestCase
+namespace BDB\Core\Exceptions;
+
+class GUIDExceptionTest extends \PHPUnit_Framework_TestCase
 {
     public function testException()
     {
         $exception = new \BDB\Core\Exceptions\GUIDException('Test');
-        $this->assertTrue($exception instanceof \BDB\Core\Exceptions\GUIDException);
+        $this->assertTrue($exception instanceof BDB\Core\Exceptions\GUIDException);
         $this->assertEquals('Test', $exception->getMessage());
         $this->assertEmpty($exception->getCode());
     }
