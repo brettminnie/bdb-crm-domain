94113810c2ba5cf6b1c627f13905e0bfe4bd9eea
Brett Minnie <github@bdb-studios.co.uk>
2014-03-30 21:44:06 +0100

    Fixed exception test

--- tests/BDBTests/Core/Exceptions/GUIDExceptionTest.php
+++ tests/BDBTests/Core/Exceptions/GUIDExceptionTest.php
@@ -7,7 +7,7 @@ class GUIDExceptionTest extends \PHPUnit_Framework_TestCase
     public function testException()
     {
         $exception = new \BDB\Core\Exceptions\GUIDException('Test');
-        $this->assertTrue($exception instanceof BDB\Core\Exceptions\GUIDException);
+        $this->assertTrue($exception instanceof \BDB\Core\Exceptions\GUIDException);
         $this->assertEquals('Test', $exception->getMessage());
         $this->assertEmpty($exception->getCode());
     }
